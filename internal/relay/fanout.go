dd5b9b5fc5a6ad27a0861eeadc0243c69991d338
lacsar712 <lacsar712@users.noreply.github.com>
2026-08-22 20:36:55 +0800

    buggy gridpulse07 classify-eq

---
+++ internal/relay/fanout.go
@@ -0,0 +1,15 @@
+package relay
+
+import "fmt"
+
+func FanoutMark(targets []string, fn func(string) error) (done bool, err error) {
+	if len(targets) == 0 {
+		return true, nil
+	}
+	for _, t := range targets {
+		if e := fn(t); e != nil {
+			return false, fmt.Errorf("%s fanout %s: %w", "gridpulse", t, e)
+		}
+	}
+	return true, nil
+}
